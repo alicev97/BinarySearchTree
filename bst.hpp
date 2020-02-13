4c5e18a3e86525ee606047d6ea0eef2c48d6396e
Alice <alice.vegliach@gmail.com>
2020-02-13 10:58:32 +0100

    find1.0

--- bst.hpp
+++ bst.hpp
@@ -43,37 +43,10 @@ public:
     template<class... Types>
     std::pair<iterator,bool> emplace(Types&&... args);
 
-        // clear
-    void clear(){
-        iterator it{head.get()};
-        while(head != nullptr){
-        while (!it.is_leaf()){
-        while (it.has_left()){
-            it.go_left();
-        }
-        while(it.has_right()){
-            it.go_right();
-        }
-        }
-        std::cout << "I should remove node :" << it->first << std::endl;
-        if(it.get_pointer() != head.get()){
-        it.go_up();
-        std::cout << "I'm gone up to:" << it.get_pointer() << std::endl;
-        if (it.has_left()){
-        it.get_pointer()->left.release();
-        } else{
-            it.get_pointer()->right.reset();
-        }
-        std::cout << "Now the node is: " << it.get_pointer()->left.get() << std::endl;
-        } else{
-            head.release();
-            std::cout << "Done" << std::endl;
-        }
-        }
-    }
-
-// begin
+    // clear
+    void clear();
     
+    // begin
     iterator begin(){
         iterator tmp{head.get()};
         while(tmp.get_pointer()->left.get() != nullptr){
@@ -101,12 +74,13 @@ public:
 
     // end
     iterator end(){return iterator{nullptr};}
-    const_iterator end() const{return const_iterator{nullptr};}
+    const_iterator end() const{std::cout << "here" << std::endl; return const_iterator{nullptr};}
     const_iterator cend() const{return const_iterator{nullptr};}
 
     // find
-    iterator find(const kT& x);
-    const_iterator find(const kT& x) const;
+   //iterator find(const kT& x);
+    template<typename OT>
+    _iterator<typename bst<kT,vT,cmp>::node_type, OT> find(const kT& x);
 
     // balance
     void balance();
@@ -125,35 +99,9 @@ public:
     //returns a reference to the pair of the head
     iterator get_head(){return iterator{head.get()};} 
     iterator get_new_parent(pair_type x);
-
 };
 
-template<typename kT, typename vT, typename cmp>
-typename bst<kT, vT, cmp>::iterator bst<kT, vT, cmp>::get_new_parent(typename bst<kT, vT, cmp>::pair_type x){
-    kT key=x.first;
-    //tmp is an iterator pointing to head and then 
-    iterator tmp{this->get_head()};
-    // while tmp is not a null ptr
-    while (tmp.is_leaf()==false){
-    if (key < tmp->first){
-        if (tmp.get_pointer()->left.get()!=NULL){
-            tmp.go_left();
-        } else { break;}
-        
-    } else if (key > tmp->first){
-        if (tmp.get_pointer()->right.get()!=NULL){
-        tmp.go_right();
-        } else {break;}
-    } else {
-        // if I've found the key
-        break;
-    }
-    }
-    
-    return tmp;
-}
-
-
+// insert
 template<typename kT,typename vT, typename cmp>
 template<typename OT>
 std::pair<typename bst<kT,vT,cmp>::iterator,bool> bst<kT,vT,cmp>::insert(OT&& x){
@@ -195,6 +143,109 @@ std::pair<typename bst<kT,vT,cmp>::iterator,bool> bst<kT,vT,cmp>::emplace(Types&
         std::pair<iterator,bool> result(it,true);
         return result;
     }   
+}
+
+template<typename kT, typename vT, typename cmp>
+void bst<kT, vT, cmp>::clear(){
+    iterator it{head.get()};
+    while(head != nullptr){
+    while (!it.is_leaf()){
+    while (it.has_left()){
+        it.go_left();
+    }
+    while(it.has_right()){
+        it.go_right();
+    }
+    }
+    std::cout << "I should remove node :" << it->first << std::endl;
+    if(it.get_pointer() != head.get()){
+        it.go_up();
+        std::cout << "I'm gone up to:" << it.get_pointer() << std::endl;
+        if (it.has_left()){
+            it.get_pointer()->left.release();
+        } else{
+            it.get_pointer()->right.reset();
+        }
+        std::cout << "Now the node is: " << it.get_pointer()->left.get() << std::endl;
+        } else{
+            head.release();
+            std::cout << "Done" << std::endl;
+        }
+    }
+}
+
+/*
+template<typename kT, typename vT, typename cmp>
+typename bst<kT, vT, cmp>::iterator bst<kT, vT, cmp>::find(const kT& x){
+
+    iterator tmp{head.get()};
+    while (x != tmp->first || !tmp.is_leaf()){
+    if (x < tmp->first){
+        if (tmp.has_left()){
+            tmp.go_left();
+        } else { break;}
+    } else if (x > tmp->first){
+        if (tmp.has_right()){
+        tmp.go_right();
+        } else {break;}
+    } else {
+        break;
+    }
+    }
+    if (x == tmp->first){
+    return tmp;
+    } else{return end();}
+}
+*/
+template<typename OT>
+template<typename kT, typename vT, typename cmp>
+_iterator<typename bst<kT,vT,cmp>::node_type, OT> bst<kT, vT, cmp>::find(const kT& x) {
+
+    std::cout << "HERE" << std::endl;
+    iterator tmp{head.get()};
+    while (x != tmp->first || !tmp.is_leaf()){
+    if (x < tmp->first){
+        if (tmp.has_left()){
+            tmp.go_left();
+        } else { break;}
+    } else if (x > tmp->first){
+        if (tmp.has_right()){
+        tmp.go_right();
+        } else {break;}
+    } else {
+        break;
+    }
+    }
+    if (x == tmp->first){
+    return OT{tmp.get_pointer()};
+    } else{return end();}
+}
+
+
+
+template<typename kT, typename vT, typename cmp>
+typename bst<kT, vT, cmp>::iterator bst<kT, vT, cmp>::get_new_parent(typename bst<kT, vT, cmp>::pair_type x){
+    kT key=x.first;
+    //tmp is an iterator pointing to head and then 
+    iterator tmp{this->get_head()};
+    // while tmp is not a null ptr
+    while (tmp.is_leaf()==false){
+    if (key < tmp->first){
+        if (tmp.get_pointer()->left.get()!=NULL){
+            tmp.go_left();
+        } else { break;}
+        
+    } else if (key > tmp->first){
+        if (tmp.get_pointer()->right.get()!=NULL){
+        tmp.go_right();
+        } else {break;}
+    } else {
+        // if I've found the key
+        break;
+    }
+    }
+    
+    return tmp;
 }
 
 template<typename KT,typename VT,typename CMP>
